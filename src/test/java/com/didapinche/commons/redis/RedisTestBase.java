a65a4acc63cce9c710b1fcb49e3a312e212bc319
fengbin <fengbin@fengbindeMacBook-Air.local>
2015-07-29 17:36:52 +0800

    update pom and test case

---
+++ src/test/java/com/didapinche/commons/redis/RedisTestBase.java
@@ -0,0 +1,32 @@
+package com.didapinche.commons.redis;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+/**
+ * Project: redis client
+ *
+ * File Created at 2015-7-29 by fengbin
+ *
+ * Copyright 2015 didapinche.com
+ */
+@ContextConfiguration(locations = {"classpath*:spring.xml"})
+public class RedisTestBase extends AbstractJUnit4SpringContextTests {
+
+
+    @Autowired
+    protected MasterSlaveRedisClient redisClient;
+
+    @Autowired
+    protected SentinelRedisClient redisSentinelClient;
+
+
+    @Before
+    public void setup(){
+
+    }
+
+
+}
