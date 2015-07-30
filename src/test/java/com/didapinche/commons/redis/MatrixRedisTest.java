465cee7db2b76c1c8d1daa79132ca0d83449eec5
fengbin <fengbin@fengbindeMacBook-Air.local>
2015-07-30 20:57:20 +0800

    add test case

--- src/test/java/com/didapinche/commons/redis/MatrixRedisTest.java
+++ src/test/java/com/didapinche/commons/redis/MatrixRedisTest.java
@@ -20,14 +20,76 @@ public class MatrixRedisTest extends RedisTestBase {
     @Autowired
     protected MatrixRedisClient matrixRedisClient;
 
+    /**
+     * master1 集群3个，master2 集群3个
+     */
     @Test
-    public void testInitPool(){
+    public void testInitPool_3_3(){
+        MatrixRedisPool pool =  (MatrixRedisPool)matrixRedisClient.getRedisPool();
+        Assert.assertNotNull(pool.getMasterShardedJedisPool());
 
+        Assert.assertTrue(pool.getSlaveShardedJedisPools().size() == 2);
+    }
+    /**
+     * master1 集群2个，master2 集群3个
+     */
+    @Test
+    public void testInitPool_2_3(){
+        MatrixRedisPool pool =  (MatrixRedisPool)matrixRedisClient.getRedisPool();
+        Assert.assertNotNull(pool.getMasterShardedJedisPool());
+
+        Assert.assertTrue(pool.getSlaveShardedJedisPools().size() == 1);
     }
 
+    /**
+     * master1 集群1个，master2 集群3个
+     */
+    @Test
+    public void testInitPool_1_3(){
+        MatrixRedisPool pool =  (MatrixRedisPool)matrixRedisClient.getRedisPool();
+        Assert.assertNotNull(pool.getMasterShardedJedisPool());
+
+        Assert.assertTrue(pool.getSlaveShardedJedisPools().size() == 0);
+    }
 
     @Test
     public void switchMaster(){
+//        MatrixRedisPool pool =  (MatrixRedisPool)matrixRedisClient.getRedisPool();
+//        ShardedJedis masterShardedJedis = pool.getMasterResource();
+//        ShardedJedis slaveShardedJedis = pool.getSlaveResource();
+//        Map<String, JedisShardInfo> masterShards =  pool.getMasterShards();
+//        Map<String, List<JedisShardInfo>> slaveShards =  pool.getMultiSlaveShards();
+//
+//        Map<String, String> masterSearchMap =  new HashMap<>();
+//        Map<String, String> slaveSearchMap =  new HashMap<>();
+//
+//        for(String masterName:masterShards.keySet()){
+//            JedisShardInfo shardInfo = masterShards.get(masterName);
+//            masterSearchMap.put(shardInfo.getHost()+":"+shardInfo.getPort(),masterName);
+//        }
+//
+//        for(String masterName:slaveShards.keySet()){
+//            List<JedisShardInfo> slaveshardInfo = slaveShards.get(masterName);
+//            for(JedisShardInfo shardInfo:slaveshardInfo){
+//                slaveSearchMap.put(shardInfo.getHost()+":"+shardInfo.getPort(),masterName);
+//            }
+//        }
+//
+//
+//        for(JedisShardInfo masterShardInfo:pool.getMasterShardedJedisPool().getResource().getAllShardInfo()){
+//            String masterMasterName = masterSearchMap.get(masterShardInfo .getHost() + ":" + masterShardInfo.getPort());
+//
+//            System.out.println(masterMasterName);
+//        }
+//
+//
+//        for(JedisShardInfo slaveShardInfo:pool.getSlaveShardedJedisPools().get(0).getResource().getAllShardInfo()){
+//            String slaveMasterName = slaveSearchMap.get(slaveShardInfo.getHost() + ":" + slaveShardInfo.getPort());
+//
+//            System.out.println(slaveMasterName);
+//        }
+//
+
 
     }
 
